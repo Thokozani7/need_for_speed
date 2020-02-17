79a5a360324e6d83ec18eb5c35b00a81f75d43c7
Thokozani Xaba <thokozanix@bbd.co.za>
2020-02-17 15:59:54 +0200

    javascript and html

---
+++ Racing_car/Racing_car/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Racing_car.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
